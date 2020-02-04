cdb6b8ac6ca5389872e6a8d8b45ff7f31fa1bdd9
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-02-04 20:54:07 +0000

    Update paths for new Dropbox layout

--- images_output.sh
+++ images_output.sh
@@ -3,4 +3,4 @@
 set -e -x
 
 mkdir -p ~/Images
-./repo_mod -o random -t "-cummins-nsfw-baby nipples" -i "~/Dropbox/Images Labelled" -i "~/Dropbox/Images Unlabelled" --link-type hard -s link_tags ~/Images
+./repo_mod -o random -t "-cummins-nsfw-baby nipples" -i "~/Dropbox/PhotoRepo/Images Labelled" -i "~/Dropbox/PhotoRepo/Images Unlabelled" --link-type hard -s link_tags ~/Images
